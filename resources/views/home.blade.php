9a373b584bde619d453f3044f72c091e206ad326
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-09 16:28:11 +0100

    print entity (oject of the class) of db in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/1200px-Netflix_icon.svg.png">
+
+
+    <title>MovieFlix</title>
 </head>
 
 <body>
-    
+    <header>
+        <h1>
+            ciao
+        </h1>
+    </header>
+    <main>
+        @foreach ($movies as $movie)
+            <p>{{$movie}}</p>
+        @endforeach
+    </main>
 </body>
 </html>
